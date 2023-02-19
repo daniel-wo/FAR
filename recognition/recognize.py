45ea4f5c9b9685a42d975a83c7872901b7428ba0
daniel-wo <d.woortmann@gmail.com>
2023-02-19 19:08:55 +0900

    change frontend location

--- recognition/recognize.py
+++ recognition/recognize.py
@@ -6,4 +6,4 @@ client = docker.from_env()
 
 
 os.system("docker run -v $(pwd)/IO" + ":/recognition/IO far " + sys.argv[1])
-webbrowser.open("../frontend/doc/examples/html_pages/01_basic.html")
+webbrowser.open("IO/0_frontend.html")
